1988247a5bd5000c58467a26455263db00712395
Alireza Bagheri <alirezabagheri12b@gmail.com>
2023-07-05 23:09:36 +0330

    interface completed and ProductRepository ctor (product list) created

--- Services/Interface/IProductRepository.cs
+++ Services/Interface/IProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,9 @@ namespace Services.Interface
 {
     internal interface IProductRepository
     {
-
+        IEnumerable<Product> GetAllProduct();
+        void Delete(int productid);
+        Product GetProductById(int id);
+        public void Edit(int id, string name, int price);
     }
 }
--- Services/ProductServices/ProductRepository.cs
+++ Services/ProductServices/ProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,19 @@ namespace Services.ProductServices
 {
     public class ProductRepository
     {
+        private List<Product> products;
+        public ProductRepository()
+        {
+            products = new List<Product>()
+            {
+                new Product(){ID = 1 ,ImgPath = "1.jpg" ,ProductName = "Chevorlet" , Price = 1000 , Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." },
+                new Product(){ID = 2 ,ImgPath = "2.jpg" , ProductName = "Ferrari" , Price = 2000 , Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+                new Product(){ID = 3 ,ImgPath = "3.jpg" , ProductName = "Audi" , Price = 3000, Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+                new Product(){ID = 4 ,ImgPath = "4.jpg" , ProductName = "Benz" , Price = 4000, Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+            };
+        }
+
+
     }
 }
+
