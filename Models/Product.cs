070cc50092bf9374d86c9af329f4808781372055
Alireza Bagheri <alirezabagheri12b@gmail.com>
2023-07-05 22:34:40 +0330

    Model added

---
+++ Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string ProductName { get; set; }
+        public Guid Barcode { get; private set; }
+        public long Price { get; set; }
+        public string ImgPath { get; set; }
+        public string Description { get; set; }
+
+        public Product()
+        {
+            Barcode = Guid.NewGuid();
+        }
+    }
+}
